a6bda473919f9e3388862e1a68a69274da001177
Andrii Snihyr <war_chief@bigmir.net>
2018-03-16 20:30:16 -0700

    Tag helpers to capture a script block and render it later

---
+++ ScriptCaptureTagHelper/ScriptCaptureTagHelper/ScriptCaptureTagHelper.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Threading.Tasks;
+
+namespace ScriptCaptureTagHelper
+{
+    /// <summary>
+    /// Captures a script block for future rendering and suppresses output
+    /// </summary>
+    [HtmlTargetElement("script", Attributes = "capture")]
+    public class ScriptCaptureTagHelper : TagHelper
+    {
+        /// <summary>
+        /// Unique id of the script block
+        /// </summary>
+        [HtmlAttributeName("capture")]
+        public string Capture { get; set; }
+
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext ViewContext { get; set; }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            if (string.IsNullOrEmpty(Capture))
+                return;
+
+            var content = await output.GetChildContentAsync();
+            var key = $"Script_{Capture}";
+            ViewContext.HttpContext.Items.Add(key, content.GetContent());
+            output.SuppressOutput();
+        }
+    }
+}
---
+++ ScriptCaptureTagHelper/ScriptCaptureTagHelper/ScriptRenderTagHelper.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace ScriptCaptureTagHelper
+{
+    /// <summary>
+    /// Renders a script block that was stored by <see cref="ScriptCaptureTagHelper"/>
+    /// </summary>
+    [HtmlTargetElement("script", Attributes = "render")]
+    public class ScriptRenderTagHelper : TagHelper
+    {
+        /// <summary>
+        /// Unique id of the script block
+        /// </summary>
+        [HtmlAttributeName("render")]
+        public string Render { get; set; }
+
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext ViewContext { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (string.IsNullOrEmpty(Render))
+                return;
+
+            var key = $"Script_{Render}";
+            if (!ViewContext.HttpContext.Items.ContainsKey(key))
+                return;
+
+            var content = ViewContext.HttpContext.Items[key].ToString();
+            output.TagName = "script";
+            output.Attributes.Clear();
+
+            output.Content.SetHtmlContent(content);
+        }
+    }
+}
