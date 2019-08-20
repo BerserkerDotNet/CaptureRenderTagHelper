f74d352499504a7a5f16914a0fb00ded131f8893
Peter <peter@ecomerc.com>
2019-08-20 14:34:30 +0200

    Awesome helper method that allows the layout view to adjust things like css classes based on captured groups

---
+++ ScriptCaptureTagHelper/Extensions/HtmlHelperExtensions.cs
@@ -0,0 +1,26 @@
+﻿using ContentCaptureTagHelper.Types;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ContentCaptureTagHelper.Extensions
+{
+    public static class HtmlHelperExtensions
+    {
+        public static bool HasCaptured(this IHtmlHelper htmlHelper, string name)
+        {            
+            var key = $"Script_{name}";
+            if (htmlHelper.ViewContext.HttpContext.Items.ContainsKey(key) &&
+                (htmlHelper.ViewContext.HttpContext.Items[key] is ContentCapture))
+                return true;
+
+            return false;
+        }
+
+        public static string IfCaptured(this IHtmlHelper htmlHelper, string name, string output)
+        {
+            if (HasCaptured(htmlHelper, name))
+                return output;
+
+            return string.Empty;
+        }
+    }
+}
