f48f74405ce5cbc5e6ca314dae0217ee08f3dda3
Andrii Snihyr <berserkerdotnet@gmail.com>
2023-11-19 19:43:14 -0800

    nuke upgrade

--- build/Build.cs
+++ build/Build.cs
@@ -1,20 +1,28 @@
+using System;
+using System.Linq;
 using Nuke.Common;
+using Nuke.Common.CI;
 using Nuke.Common.Execution;
 using Nuke.Common.Git;
+using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
 using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Tools.GitVersion;
+using Nuke.Common.Tools.NerdbankGitVersioning;
 using Nuke.Common.Utilities.Collections;
 using static Nuke.Common.IO.FileSystemTasks;
-using static Nuke.Common.IO.PathConstruction;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
-[CheckBuildProjectConfigurations]
-[UnsetVisualStudioEnvironmentVariables]
 class Build : NukeBuild
 {
-    public static int Main () => Execute<Build>(x => x.Package);
+    /// Support plugins are available for:
+    ///   - JetBrains ReSharper        https://nuke.build/resharper
+    ///   - JetBrains Rider            https://nuke.build/rider
+    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
+    ///   - Microsoft VSCode           https://nuke.build/vscode
+
+    public static int Main () => Execute<Build>(x => x.Compile);
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
@@ -24,15 +32,15 @@ class Build : NukeBuild
 
     [Parameter] 
     readonly string Source = "https://api.nuget.org/v3/index.json";
-
+    
     [Solution] readonly Solution Solution;
     [GitRepository] readonly GitRepository GitRepository;
-    [GitVersion] readonly GitVersion GitVersion;
+    [NerdbankGitVersioning] readonly NerdbankGitVersioning GitVersion;
 
     AbsolutePath SourceDirectory => RootDirectory / "src";
     AbsolutePath TestsDirectory => RootDirectory / "tests";
     AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
-
+    
     Target Clean => _ => _
         .Before(Restore)
         .Executes(() =>
@@ -56,9 +64,9 @@ class Build : NukeBuild
             DotNetBuild(s => s
                 .SetProjectFile(Solution)
                 .SetConfiguration(Configuration)
-                .SetAssemblyVersion(GitVersion.GetNormalizedAssemblyVersion())
-                .SetFileVersion(GitVersion.GetNormalizedFileVersion())
-                .SetInformationalVersion(GitVersion.InformationalVersion)
+                .SetAssemblyVersion(GitVersion.SimpleVersion)
+                .SetFileVersion(GitVersion.SimpleVersion)
+                .SetInformationalVersion(GitVersion.SimpleVersion)
                 .EnableNoRestore());
         });
 
@@ -86,4 +94,5 @@ class Build : NukeBuild
                 .EnableIncludeSymbols()
                 .SetOutputDirectory(ArtifactsDirectory));
         });
+
 }
